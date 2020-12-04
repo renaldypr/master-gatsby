330839c438e36c50e8572ad74cb46ff0075cbfcb
Renaldy Pratama <renaldy.pratama@bizzy.co.id>
2020-12-04 10:14:56 +0700

    Module #2 done

---
+++ gatsby/src/pages/beers.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function BeersPage() {
+  return (
+    <>
+      <p>Hey! I'm the Beers page</p>
+    </>
+  );
+}
---
+++ gatsby/src/pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function HomePage() {
+  return (
+    <>
+      <p>Hey! I'm the Home Page</p>
+    </>
+  );
+}
