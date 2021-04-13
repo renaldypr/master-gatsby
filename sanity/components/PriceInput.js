837ab91cdcea1d7d737ac06b3f26c4ff4c236627
Renaldy Pratama <renaldy.pratama@bizzy.co.id>
2021-04-13 14:31:25 +0700

    Finish Module #4

---
+++ sanity/components/PriceInput.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PatchEvent, { set, unset } from 'part:@sanity/form-builder/patch-event';
+
+function createPatchFrom(value) {
+  return PatchEvent.from(value === '' ? unset() : set(Number(value)));
+}
+
+const formatMoney = Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+}).format;
+
+export default function PriceInput({ type, value, onChange, inputComponent }) {
+  return (
+    <div>
+      <h2>
+        {type.title} - {value ? formatMoney(value / 100) : ''}
+      </h2>
+      <p>{type.description}</p>
+      <input
+        type={type.name}
+        value={value}
+        onChange={(event) => onChange(createPatchFrom(event.target.value))}
+        ref={inputComponent}
+      />
+    </div>
+  );
+}
